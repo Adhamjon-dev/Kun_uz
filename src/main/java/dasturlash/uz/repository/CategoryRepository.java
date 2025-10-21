29d512a9c301c857127faeae3ca4731b3568288f
Adhamjon-dev <adhamjonyaxshiboyev@gmail.com>
2025-10-21 20:28:46 +0500

    category started

---
+++ src/main/java/dasturlash/uz/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package dasturlash.uz.repository;
+
+import dasturlash.uz.entitiy.CategoryEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<CategoryEntity, Integer> {
+}
---
+++ src/main/java/dasturlash/uz/service/CategoryService.java
@@ -0,0 +1,11 @@
+package dasturlash.uz.service;
+
+import dasturlash.uz.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+}
